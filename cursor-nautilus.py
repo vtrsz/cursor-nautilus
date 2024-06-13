e0fc0c5bd1d5b79fd503c4f617ed3ca4fec31768
Vitor Souza <vitorsouzaribeiro1@gmail.com>
2024-06-13 15:18:11 -0300

    possible fix

--- cursor-nautilus.py
+++ cursor-nautilus.py
@@ -37,7 +37,7 @@ class CursorExtension(GObject.GObject, Nautilus.MenuProvider):
         if NEWWINDOW:
             args = '--new-window '
 
-        call(PATH + ' ' + args + safepaths + '&', shell=True)
+        call(PATH + ' ' + args + safepaths)
 
     def get_file_items(self, *args):
         files = args[-1]
